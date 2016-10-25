bfe82a2a60fcde01c20c4281226f46e2bfa3787a
guanyi.bei <guanyi.bei@cyphertech.cn>
2016-10-25 17:00:58 +0800

    Signed-off-by: guanyi.bei <guanyi.bei@cyphertech.cn>

--- src/components/example2.vue
+++ src/components/example2.vue
@@ -1,10 +1,12 @@
 <template>
 	<div>
-		<cell title="example2" link="/example2">
-		</cell>
-		<cell title="example1" value="Protected" link="/index">
-		</cell>
-		<cell title="Notifications" value="Enabled"></cell>
+		<group>
+			<cell title="example2" link="/example2">
+			</cell>
+			<cell title="example1" value="Protected" link="/index">
+			</cell>
+			<cell title="Notifications" value="Enabled"></cell>
+		</group>
 	</div>
 </template>
 <script>
