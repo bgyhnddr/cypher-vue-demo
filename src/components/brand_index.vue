6899dd49084e1bfa10855827763e23a42249f437
bili.ou <bili.ou@cyphertech.cn>
2016-10-27 10:15:11 +0800

    添加 品牌商首页

---
+++ src/components/brand_index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+		<group>
+			<cell title="新代理加盟" value=">" inline-desc="查看或生成申请下级代理的分享链接"
+                 @click="showModel">
+               <span class="demo-icon" slot="icon" style="color:#F70968">&#xe633;</span>
+			</cell>
+		</group>
+	</div>
+</template>
+
+<script>
+    import {
+        Cell,
+        Group
+    } from 'vux'
+    export default {
+        components: {
+            Cell,
+            Group
+        },
+        data() {
+            return {}
+        },
+        methods:{
+            showModel() {
+                console.log("打开选择代理弹出层")
+            }
+        }
+    }
+</script>
