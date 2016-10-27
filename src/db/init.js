03ef72ebf23f40f66f08ce4d6900d31e00ecbb10
guanyi.bei <guanyi.bei@cyphertech.cn>
2016-10-27 10:10:02 +0800

    change

--- src/db/init.js
+++ src/db/init.js
@@ -7,19 +7,11 @@ module.exports = function(req, res, next) {
         require('./models/user_role'),
         require('./models/role_permission'),
         require('./models/attachment'),
-        require('./models/file'),
-        require('./models/brand'),
-        require('./models/brand_detail'),
-        require('./models/agent'),
-        require('./models/agent_detail'),
-        require('./models/employment'),
-        require('./models/employment_detail'),
-        require('./models/brand_employment_meta'),
-        require('./models/agent_brand_roles'),
-        require('./models/brand_role'),
-        require('./models/employable_rules')
+        require('./models/file')
     ].map((o) => o.sync({ force: true }))).then(function() {
         return require('./init_data')()
+    }).then(() => {
+        return require('./init_app_data')()
     }).then(function() {
         res.send("success")
     }).catch(function(err) {
