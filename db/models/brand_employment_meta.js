6a1ae896c14a262a191dd84c96b1acc8a9a4d118
bili.ou <bili.ou@cyphertech.cn>
2016-11-01 17:44:04 +0800

    Signed-off-by: bili.ou <bili.ou@cyphertech.cn>

--- db/models/brand_employment_meta.js
+++ db/models/brand_employment_meta.js
@@ -4,12 +4,11 @@ var sequelize = require('../sequelize')
 var brand_employment_meta = sequelize.define(
     'brand_employment_meta', {
         id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, unique: true },
-        brand_guid: Sequelize.STRING,
+        brand_guid: Sequelize.STRING, 
         key: Sequelize.STRING,
-        type: Sequelize.STRING,
-        required: Sequelize.BOOLEAN,
+        type: Sequelize.STRING
     }, {
-        underscored: true
+        underscored: true  
     })
 
 
