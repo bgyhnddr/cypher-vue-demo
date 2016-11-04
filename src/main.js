a441b386d6026bd0a32a49077d6038dee31ef8a6
limt <921136839@qq.com>
2016-11-04 16:54:53 +0800

    Signed-off-by: limt <921136839@qq.com>

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { dateformat } from './extend/date-format'
 import timei from './extend/vue-resource-timeout'
 import authCallback from './extend/auth-callback'
 
+import authAPI from './api/auth'
+
 Vue.use(require('vue-resource'))
 Vue.http.interceptors.push(timei)
 Vue.http.interceptors.push(authCallback)
@@ -34,7 +36,45 @@ const router = new VueRouter({
 configRouter(router)
 
 router.beforeEach((tran) => {
-    tran.next();
+    var path = tran.to.path
+    authAPI.getUser().then(function(result) {
+        function contains(obj) {
+            var i = result.permissions.length;
+            while (i--) {
+                if (result.permissions[i] === obj) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        if (result.name) {
+            switch (path.split('?')[0]) {
+                case "/employManagement":
+                    tran.next()
+                    break
+                case "/employManagement/audit":
+                case "/employManagement/auditInfo":
+                case "/employManagement/employmentHistory":
+                    if (contains("employment")) {
+                        tran.next()
+                    } else {
+                        window.alert('权限不足')
+                        router.go({ path: path.replace(path, '/employManagement') })
+                    }
+                    break
+                case "/index":
+                case "/auth/login":
+                    router.go('employManagement')
+                    break
+            }
+        } else {
+            if (path == '/index' || path == '/auth/login') {
+                tran.next()
+            } else {
+                router.go({ path: path.replace(path, '/index') })
+            }
+        }
+    })
 })
 
 router.start(App, 'app');
