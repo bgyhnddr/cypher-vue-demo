7dfd94da95c0e60f66e03f89ac04d96498ef83ed
bili.ou <bili.ou@cyphertech.cn>
2016-11-08 10:41:30 +0800

    添加 九宫格后的搜索页

---
+++ src/components/HomePageSearch.vue
@@ -0,0 +1,31 @@
+<template>
+    <p>查找</p>
+   
+</template>
+
+<script>
+    import {
+        Group,
+        XInput
+    } from 'vux'
+    import authAPI from '../api/auth'
+    import employmentAPI from '../api/employment'
+
+
+    export default {
+        components: {
+            Group,
+            XInput
+        },
+        data() {
+            return {
+                keyword: null,
+            }
+        },
+        methods: {},
+        ready() {}
+    }
+</script>
+<style lang="less">
+
+</style>
