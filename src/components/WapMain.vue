c162de8c9cda44ec72e2732f8c1d3bd948302e2d
limt <921136839@qq.com>
2016-11-11 10:15:16 +0800

    Signed-off-by: limt <921136839@qq.com>

--- src/components/WapMain.vue
+++ src/components/WapMain.vue
@@ -5,8 +5,8 @@
             <!--header slot-->
             <div class="vux-demo-header-box" slot="header">
                 <x-header :left-options="leftOptions" transition="headerTransition" :title="title" @on-click-title="scrollTop"></x-header>
-                <div slot="left">
-                    <button v-if="ShowBack" @click="onClickBack">返回</button>
+                <div slot="left" v-if="ShowBack">
+                    <button @click="onClickBack">返回</button>
                 </div>
             </div>
             <!--default slot-->
