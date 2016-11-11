9483727c7658eeba6f4488b822fd9c4c70b6c6e5
shihao.guo <shihao.guo@cyphertech.cn>
2016-11-11 18:56:19 +0800

    修改

--- src/components/CurrentEmploymentList.vue
+++ src/components/CurrentEmploymentList.vue
@@ -1,16 +1,16 @@
 ﻿<template>
-  <div class="">
+  <div class="current-bac">
     <group>
       <selector title="" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;--排序--" :options="selectList" @on-change="onChange"></selector>
-    </group>
+    </group></div>
 <div class="current-list">
     <group>
          <a v-for="item in data" class="weui_cell a-li a-li-first current-list-list"  v-link="{path: '/employManagement/currentInfo/'+ item.guid}" >
             <div class="weui_cell_bd weui_cell_primary">
                 <span>招募等级：{{item.brand_role.name}}</span>
-                <p></p>
+               
                 <span>发起时间：{{item.create_time}}</span>
-                <p></p>
+                
                 <span>剩余时间：{{calculateRemainingTime(item)}}</span>
             </div>
          </a>
@@ -151,9 +151,10 @@
 }
 .current-list .weui_cell_bd.weui_cell_primary span{
 
-    line-height: 1.8em;
+       line-height: 1.7em;
     font-size: 14px;
     font-family: "微软雅黑";
+    display: block;
 
 }
 .current-list .weui_cell_bd.weui_cell_primary span:nth-chlid(1){
@@ -161,5 +162,57 @@
     line-height: 1.3em;
 
 
+}
+.current-list .weui_cell_bd.weui_cell_primary:after{
+       content: " ";
+    display: inline-block;
+    -webkit-transform: rotate(45deg);
+    transform: rotate(45deg);
+    height: 8px;
+    width: 8px;
+    border-width: 1px 1px 0 0;
+    border-color: #686f78;
+    border-style: solid;
+    position: relative;
+    top: -2px;
+    position: absolute;
+    top: 50%;
+    right: 15px;
+    margin-top: -3px;
+}
+
+.current-bac .weui_cells.vux-no-group-title{
+
+position: absolute;
+    z-index: 1000;
+       top: 12px;
+    right: 16px;
+
+
+
+}
+
+.current-bac .weui_cell.weui_cell_select {
+    padding: 0;
+}
+.current-bac .weui_cell_select .weui_select{
+    height: 23px;
+    line-height: 22px;
+    font-size: 13px;
+    font-family: "微软雅黑";
+padding-right: 14px;
+    background: #fff;
+border: 1px solid #d3d1d1;
+    padding-left: 1px;
+
+}
+.current-bac .weui_cell_select .weui_cell_bd:after{
+
+    transform: rotate(135deg);
+    top: 44%;
+    right: 5px;
+    z-index: 1;
+    height: 4px;
+    width: 4px;
 }
 </style>
