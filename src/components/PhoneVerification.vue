0304d553397f20793a82d34e7579cd761f02d170
limt <921136839@qq.com>
2016-11-14 17:07:00 +0800

    Signed-off-by: limt <921136839@qq.com>

--- src/components/PhoneVerification.vue
+++ src/components/PhoneVerification.vue
@@ -5,7 +5,7 @@
                 <group v-if="!UserPhone">
                     <x-input title="手机号" :value.sync="cellphone" placeholder="请输入手机号" is-type="china-mobile" v-ref:cellphone></x-input>
                 </group>
-                <p v-if="UserPhone">请输入{{cellphone}}短信验证码</p>
+                <p v-if="UserPhone">请输入{{GetPhone}}短信验证码</p>
                 <!--<group>
                     <x-input title="验证码" :value.sync="VerificationCode" placeholder="请输入验证码"></x-input>
                 </group>-->
@@ -46,6 +46,7 @@
         data() {
             return {
                 TimeLeft: "",
+                GetPhone: "",
                 cellphone: "",
                 VerificationCode: "",
                 UserPhone: false,
@@ -134,7 +135,7 @@
                 var that = this
                 that.UserPhone = true
                 authAPI.getUser().then(function(result) {
-                    that.GetPhone = result.name
+                    that.GetPhone = result.name.substring(0,3)+'****'+result.name.substring(8,11)
                     that.cellphone = result.name
                 })
             }
@@ -182,26 +183,20 @@
         font-size: 16px;
         color: #f43530;
     }
-
-   .phone-button   button.weui_btn.weui_btn_default{
-
-color: #80c269;
-    font-size: 14px;
-    font-family: "微软雅黑";
-    border: 0;
-    background: 0;
-
-
-}
-
-
-   .phone-button  .weui_btn:after{
-
-    border: 0;
-    height: 62px;
-    margin-top: 7%;
-    border-left: 1px solid #d3d1d1;
-    border-radius: 0;
-
-}
+    
+    .phone-button button.weui_btn.weui_btn_default {
+        color: #80c269;
+        font-size: 14px;
+        font-family: "微软雅黑";
+        border: 0;
+        background: 0;
+    }
+    
+    .phone-button .weui_btn:after {
+        border: 0;
+        height: 62px;
+        margin-top: 7%;
+        border-left: 1px solid #d3d1d1;
+        border-radius: 0;
+    }
 </style>
