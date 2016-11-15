b4917bb63eddf59f5ac13f4e258e2f2662b20f45
bili.ou <bili.ou@cyphertech.cn>
2016-11-15 14:38:53 +0800

    修改 分享招募链接，不能进入资料填写页面

--- src/components/CurrentEmploymentList.vue
+++ src/components/CurrentEmploymentList.vue
@@ -1,22 +1,24 @@
 ﻿<template>
-  <div class="current-bac">
-    <group>
-      <selector title="" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;--排序--" :options="selectList" @on-change="onChange"></selector>
-    </group></div>
-<div class="current-list">
-    <group>
-         <a v-for="item in data" class="weui_cell a-li a-li-first current-list-list"  v-link="{path: '/employManagement/currentInfo/'+ item.guid}" >
-            <div class="weui_cell_bd weui_cell_primary">
-                <span>招募等级：{{item.brand_role.name}}</span>
-               
-                <span>发起时间：{{item.create_time}}</span>
-                
-                <span>剩余时间：{{calculateRemainingTime(item)}}</span>
-            </div>
-         </a>
-    </group>
-    <alert :show.sync="showMsg" button-text="确认">{{errorMsg}}</alert>
-</div>
+<div>
+    <div class="current-bac">
+        <group>
+            <selector title="" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;--排序--" :options="selectList" @on-change="onChange"></selector>
+        </group>
+    </div>
+    <div class="current-list">
+        <group>
+            <a v-for="item in data" class="weui_cell a-li a-li-first current-list-list" v-link="{path: '/employManagement/currentInfo/'+ item.guid}">
+                <div class="weui_cell_bd weui_cell_primary">
+                    <span>招募等级：{{item.brand_role.name}}</span>
+
+                    <span>发起时间：{{item.create_time}}</span>
+
+                    <span>剩余时间：{{calculateRemainingTime(item)}}</span>
+                </div>
+            </a>
+        </group>
+        <alert :show.sync="showMsg" button-text="确认">{{errorMsg}}</alert>
+    </div>
 </div>
 </template>
 
@@ -129,87 +131,77 @@
     }
 </script>
 <style>
-.current-list a.current-list-list {
-    background: #fff;
-    margin: 3% 0;
-    border-top: 1px solid #d3d1d1;
-    border-bottom: 1px solid #d3d1d1;
-    position: relative;
-    padding: 3px 15px 3px 15px;
-    line-height: 1.2em;
-}
-
-.current-list .weui_cell:before{
-
-  border-top:0;
-
-
-
-}
-.current-list .weui_cell_bd.weui_cell_primary span{
-
-       line-height: 1.7em;
-    font-size: 14px;
-    font-family: "微软雅黑";
-    display: block;
-
-}
-.current-list .weui_cell_bd.weui_cell_primary span:nth-chlid(1){
-
-    line-height: 1.3em;
-
-
-}
-.current-list .weui_cell_bd.weui_cell_primary:after{
-       content: " ";
-    display: inline-block;
-    -webkit-transform: rotate(45deg);
-    transform: rotate(45deg);
-    height: 8px;
-    width: 8px;
-    border-width: 1px 1px 0 0;
-    border-color: #686f78;
-    border-style: solid;
-    position: relative;
-    top: -2px;
-    position: absolute;
-    top: 50%;
-    right: 15px;
-    margin-top: -3px;
-}
-
-.current-bac .weui_cells.vux-no-group-title{
-
-position: absolute;
-    z-index: 1000;
-       top: 12px;
-    right: 16px;
-
-
-
-}
-
-.current-bac .weui_cell.weui_cell_select {
-    padding: 0;
-}
-.current-bac .weui_cell_select .weui_select{
-    height: 23px;
-    line-height: 22px;
-    font-size: 13px;
-    font-family: "微软雅黑";
-padding-right: 14px;
-    background: #fff;
-border: 1px solid #d3d1d1;
-    padding-left: 1px;
-
-}
-.current-bac .weui_cell_select .weui_cell_bd:after{
-
-    transform: rotate(135deg);
-    top: 44%;
-    right: 5px;
-    z-index: 1;
-    height: 4px;
-    width: 4px;
-}
+    .current-list a.current-list-list {
+        background: #fff;
+        margin: 3% 0;
+        border-top: 1px solid #d3d1d1;
+        border-bottom: 1px solid #d3d1d1;
+        position: relative;
+        padding: 3px 15px 3px 15px;
+        line-height: 1.2em;
+    }
+    
+    .current-list .weui_cell:before {
+        border-top: 0;
+    }
+    
+    .current-list .weui_cell_bd.weui_cell_primary span {
+        line-height: 1.7em;
+        font-size: 14px;
+        font-family: "微软雅黑";
+        display: block;
+    }
+    
+    .current-list .weui_cell_bd.weui_cell_primary span:nth-chlid(1) {
+        line-height: 1.3em;
+    }
+    
+    .current-list .weui_cell_bd.weui_cell_primary:after {
+        content: " ";
+        display: inline-block;
+        -webkit-transform: rotate(45deg);
+        transform: rotate(45deg);
+        height: 8px;
+        width: 8px;
+        border-width: 1px 1px 0 0;
+        border-color: #686f78;
+        border-style: solid;
+        position: relative;
+        top: -2px;
+        position: absolute;
+        top: 50%;
+        right: 15px;
+        margin-top: -3px;
+    }
+    
+    .current-bac .weui_cells.vux-no-group-title {
+        position: absolute;
+        z-index: 1000;
+        top: 12px;
+        right: 16px;
+    }
+    
+    .current-bac .weui_cell.weui_cell_select {
+        padding: 0;
+    }
+    
+    .current-bac .weui_cell_select .weui_select {
+        height: 23px;
+        line-height: 22px;
+        font-size: 13px;
+        font-family: "微软雅黑";
+        padding-right: 14px;
+        background: #fff;
+        border: 1px solid #d3d1d1;
+        padding-left: 1px;
+    }
+    
+    .current-bac .weui_cell_select .weui_cell_bd:after {
+        transform: rotate(135deg);
+        top: 44%;
+        right: 5px;
+        z-index: 1;
+        height: 4px;
+        width: 4px;
+    }
 </style>
