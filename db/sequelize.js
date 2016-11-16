cb87351820bc6926063095e08a341bf5c7816bbb
hao.wang <hao.wang@cyphertech>
2016-11-16 11:40:11 +0800

    改数据库配置

--- db/sequelize.js
+++ db/sequelize.js
@@ -4,9 +4,9 @@ var fs = require('fs')
 if (!fs.existsSync("db/data")) {
     fs.mkdirSync("db/data")
 }
-var sequelize = new Sequelize('database', null, null, {
-    dialect: 'sqlite',
-    storage: 'db/data/database.db',
-    logging: false
+var sequelize = new Sequelize('bleman', 'cypher_admin', 'Cypher2015', {
+    dialect: 'mysql',
+    host: 'rdssl1u0dsbhp7obuinml.mysql.rds.aliyuncs.com',
+    port: 3306
 })
 module.exports = sequelize
