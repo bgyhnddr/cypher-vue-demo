064fd68419d49a20e52d738d4fcf97a1f1f9c6ab
bili.ou <bili.ou@cyphertech.cn>
2016-11-21 13:05:25 +0800

    修改微信号内数据库更新逻辑

--- server/private/changeUserInfo.js
+++ server/private/changeUserInfo.js
@@ -1,7 +1,7 @@
 var exec = {
   checkPwd(req, res, next) {
     var pwd = req.body.pwd
-    var user_account = req.body.user_account
+    var user_account = req.session.userInfo.name
 
     var user = require('../../db/models/user')
 
@@ -19,47 +19,54 @@ var exec = {
     })
   },
   changeWechat(req, res, next) {
-    var user_account = req.body.user_account
+    var user_account = req.session.userInfo.name
     var wechat = req.body.wechat
 
+    var user = require('../../db/models/user')
     var agent = require('../../db/models/agent')
     var agent_detail = require('../../db/models/agent_detail')
 
-    return agent_detail.findOne({
+    user.hasOne(agent)
+    agent.hasMany(agent_detail)
+
+    return agent_detail.findAll({
       where: {
-        key: "wechat",
-        value: wechat
+        $and: {
+          key: "wechat",
+          value: wechat
+        }
       }
-    }).then(function(result) {
-      if (result != null) {
-        return Promise.reject("该微信号已被注册，请重新输入")
+    }).then((result) => {
+      if (result.length > 0) {
+        return Promise.reject("该微信号已存在")
       } else {
-        return agent.findOne({
-          where: {
-            user_account: user_account
-          }
-        }).then(function(result) {
-          if (result == null) {
-            return Promise.reject("修改微信号失败")
-          } else {
-            return agent_detail.findOne({
-              where: {
-                agent_guid: result.guid,
-                key: 'wechat'
-              }
-            }).then(function(result) {
-              if (result == null) {
-                return Promise.reject("修改微信号失败")
-              } else {
-                result.value = wechat
-                return result.save()
-              }
-            }).then(function(result) {
-              return true
-            })
-          }
-        })
+        return result
       }
+    }).then(() => {
+      return user.findOne({
+        include: {
+          model: agent,
+          include: {
+            model: agent_detail,
+            where: {
+              key: "wechat"
+            }
+          }
+        },
+        where: {
+          account: user_account
+        }
+      })
+    }).then((result) => {
+      if (result != null) {
+        var ad = result.agent.agent_details[0]
+        ad.value = wechat
+        return ad.save()
+      } else {
+        return Promise.reject("查找个人信息出错，请再尝试")
+      }
+    }).then(function() {
+      return true
     })
   },
   changeCellphone(req, res, next) {
@@ -82,7 +89,7 @@ var exec = {
       }
     }).then((result) => {
       if (result.length > 0) {
-        return Promise.reject("电话已存在")
+        return Promise.reject("该手机号已存在")
       } else {
         return result
       }
