ae7db13df78dc6782588d0f1628faa78cdd70c2c
bili.ou <bili.ou@cyphertech.cn>
2016-11-22 18:30:14 +0800

    修改 checkBeforeSubmit和submitApplication中 判断错误

--- server/public/applyEmployment.js
+++ server/public/applyEmployment.js
@@ -1,138 +1,125 @@
 var exec = {
-    getBrandInfo(req, res, next) {
-      var user_account = req.body.user_account
+  getBrandInfo(req, res, next) {
+    var user_account = req.body.user_account
 
-      var agent = require('../../db/models/agent')
-      var agent_brand_role = require('../../db/models/agent_brand_role')
-      var brand_role = require('../../db/models/brand_role')
-      var brand = require('../../db/models/brand')
-      var brand_detail = require('../../db/models/brand_detail')
+    var agent = require('../../db/models/agent')
+    var agent_brand_role = require('../../db/models/agent_brand_role')
+    var brand_role = require('../../db/models/brand_role')
+    var brand = require('../../db/models/brand')
+    var brand_detail = require('../../db/models/brand_detail')
 
-      brand.hasOne(brand_role)
-      brand_role.hasOne(agent_brand_role)
-      agent_brand_role.belongsTo(agent)
-      brand.hasMany(brand_detail)
+    brand.hasOne(brand_role)
+    brand_role.hasOne(agent_brand_role)
+    agent_brand_role.belongsTo(agent)
+    brand.hasMany(brand_detail)
 
-      return brand.findOne({
+    return brand.findOne({
+      include: [{
+        model: brand_role,
         include: [{
-          model: brand_role,
+          model: agent_brand_role,
           include: [{
-            model: agent_brand_role,
-            include: [{
-              model: agent,
-              where: {
-                user_account: user_account
-              },
-            }],
+            model: agent,
+            where: {
+              user_account: user_account
+            },
           }],
-        }]
-      }).then(function(result) {
-        if (result == null) {
-          return Promise.reject("找不到您的品牌商角色")
-        } else {
-          return brand.findOne({
-            where: {
-              guid: result.guid,
-            },
-            include: [{
-              model: brand_detail,
-            }],
-          }).then(function(result) {
-            if (result == null) {
-              return Promise.reject("找不到您的品牌商资料")
-            } else {
-              return result
-            }
-          })
-        }
-      })
-    },
-    getRoleName(req, res, next) {
-      var brand_guid = req.body.brand_guid
-      var brand_role_code = req.body.brand_role_code
-
-      var brand_role = require('../../db/models/brand_role')
-
-      return brand_role.findOne({
-        where: {
-          brand_guid: brand_guid,
-          code: brand_role_code
-        }
-      }).then(function(result) {
-        if (result == null) {
-          return Promise.reject("品牌商角色读取出错")
-        } else {
-          return result
-        }
-      })
-
-    },
-    getAgentInfo(req, res, next) {
-      var user_account = req.body.user_account
-
-      var agent = require('../../db/models/agent')
-      var agent_detail = require('../../db/models/agent_detail')
-
-      agent.hasMany(agent_detail)
-
-      return agent.findOne({
-        where: {
-          user_account: user_account
-        },
-        include: [{
-          model: agent_detail
-        }]
-      }).then(function(result) {
-        if (result == null) {
-          return Promise.reject("上级授权角色资料读取出错")
-        } else {
-          return result
-        }
-      })
-    },
-    submitApplication(req, res, next) {
-      var meta = req.body.meta
-      var data = req.body.data
-      var employmentData = req.body.employmentData
-      var deadline = req.body.deadline
-      var publishEmploymentGuid = req.body.publishEmploymentGuid
-
-      var uuid = require('node-uuid')
-      var guid = uuid.v1()
-
-      var employment = require('../../db/models/employment')
-      var employment_detail = require('../../db/models/employment_detail')
-      var brand_role = require('../../db/models/brand_role')
-      var user = require('../../db/models/user')
-      var user_role = require('../../db/models/user_role')
-      var agent = require('../../db/models/agent')
-      var agent_detail = require('../../db/models/agent_detail')
-      var agent_brand_role = require('../../db/models/agent_brand_role')
-      var sequelize = require('../../db/sequelize')
-      var publish_employment = require('../../db/models/publish_employment')
-
-      agent.hasOne(agent_brand_role)
-      agent_brand_role.belongsTo(brand_role)
-      employment.hasOne(employment_detail)
-
-      return Promise.all([
-        //查找是否已提交招募申请,未通过
-        employment.findOne({
+        }],
+      }]
+    }).then(function(result) {
+      if (result == null) {
+        return Promise.reject("找不到您的品牌商角色")
+      } else {
+        return brand.findOne({
+          where: {
+            guid: result.guid,
+          },
           include: [{
-            model: employment_detail,
-            where: {
-              $or: [{
-                key: "cellphone",
-                value: data.cellphone
-              }, {
-                key: "wechat",
-                value: data.wechat
-              }]
-            },
-          }]
-        }),
-        //查找是否已注册成为代理
-        agent_detail.findOne({
+            model: brand_detail,
+          }],
+        }).then(function(result) {
+          if (result == null) {
+            return Promise.reject("找不到您的品牌商资料")
+          } else {
+            return result
+          }
+        })
+      }
+    })
+  },
+  getRoleName(req, res, next) {
+    var brand_guid = req.body.brand_guid
+    var brand_role_code = req.body.brand_role_code
+
+    var brand_role = require('../../db/models/brand_role')
+
+    return brand_role.findOne({
+      where: {
+        brand_guid: brand_guid,
+        code: brand_role_code
+      }
+    }).then(function(result) {
+      if (result == null) {
+        return Promise.reject("品牌商角色读取出错")
+      } else {
+        return result
+      }
+    })
+
+  },
+  getAgentInfo(req, res, next) {
+    var user_account = req.body.user_account
+
+    var agent = require('../../db/models/agent')
+    var agent_detail = require('../../db/models/agent_detail')
+
+    agent.hasMany(agent_detail)
+
+    return agent.findOne({
+      where: {
+        user_account: user_account
+      },
+      include: [{
+        model: agent_detail
+      }]
+    }).then(function(result) {
+      if (result == null) {
+        return Promise.reject("上级授权角色资料读取出错")
+      } else {
+        return result
+      }
+    })
+  },
+  submitApplication(req, res, next) {
+    var meta = req.body.meta
+    var data = req.body.data
+    var employmentData = req.body.employmentData
+    var deadline = req.body.deadline
+    var publishEmploymentGuid = req.body.publishEmploymentGuid
+
+    var uuid = require('node-uuid')
+    var guid = uuid.v1()
+
+    var employment = require('../../db/models/employment')
+    var employment_detail = require('../../db/models/employment_detail')
+    var brand_role = require('../../db/models/brand_role')
+    var user = require('../../db/models/user')
+    var user_role = require('../../db/models/user_role')
+    var agent = require('../../db/models/agent')
+    var agent_detail = require('../../db/models/agent_detail')
+    var agent_brand_role = require('../../db/models/agent_brand_role')
+    var sequelize = require('../../db/sequelize')
+    var publish_employment = require('../../db/models/publish_employment')
+
+    agent.hasOne(agent_brand_role)
+    agent_brand_role.belongsTo(brand_role)
+    employment.hasOne(employment_detail)
+
+    return Promise.all([
+      //查找是否已提交招募申请,未通过
+      employment.findOne({
+        include: [{
+          model: employment_detail,
           where: {
             $or: [{
               key: "cellphone",
@@ -141,135 +128,149 @@ var exec = {
               key: "wechat",
               value: data.wechat
             }]
-          }
-        }),
-
-        //查找品牌商account
-        agent.findOne({
-          include: [{
-            model: agent_brand_role,
-            include: [{
-              model: brand_role,
-              where: {
-                level: "0"
-              },
-            }],
+          },
+        }]
+      }),
+      //查找是否已注册成为代理
+      agent_detail.findOne({
+        where: {
+          $or: [{
+            key: "cellphone",
+            value: data.cellphone
+          }, {
+            key: "wechat",
+            value: data.wechat
           }]
-        }),
+        }
+      }),
 
-        //查找发起招募信息
-        publish_employment.findOne({
-          where: {
-            guid: publishEmploymentGuid
-          }
-        })
-      ]).then(function(result) {
-          var overtime = false
+      //查找品牌商account
+      agent.findOne({
+        include: [{
+          model: agent_brand_role,
+          include: [{
+            model: brand_role,
+            where: {
+              level: "0"
+            },
+          }],
+        }]
+      }),
 
-          if (result[3] != null) {
-            var startDate = new Date(result[3].create_time)
-            var endDate = new Date(startDate.getTime() + 2 * 3600 * 1000)
-            if (endDate <= new Date()) {
-              overtime = true
-            }
-          }
-
-          if (result[0] != null) {
-            if (result[0].status == "未审核" || result[0].audit_result == "已通过") {
-              return Promise.reject("您已提交申请或已成为该品牌成员，申请失败")
-            }
-          } else if (result[1] != null) {
-            return Promise.reject("您已提交申请或已成为该品牌成员，申请失败")
-          } else if (result[2] == null) {
-            return Promise.reject("招募信息读取出错")
-          } else if (result[3] == null) {
-            return Promise.reject("招募信息读取出错")
-          } else if (result[3].status == false || overtime) {
-            return Promise.reject("招募已关闭，请关闭本页面")
-          } else {
-            var pwd = "";
-            for (var i = 0; i < 8; i++) {
-              pwd += Math.floor(Math.random() * 10);
-            }
-
-            var audit_user_account = result[2].user_account
-
-            return sequelize.transaction().then(function(t) {
-                return Promise.all([
-                  user.create({
-                    account: guid,
-                    password: pwd
-                  }, {
-                    transaction: t
-                  }),
-                  user_role.create({
-                    user_account: guid,
-                    role_code: 'user'
-                  }, {
-                    transaction: t
-                  }),
-                  employment.create({
-                    guid: employmentData.guid,
-                    publish_employment_guid: employmentData.publishEmploymentInfo.guid,
-                    employer_user_account: employmentData.publishEmploymentInfo.employer_user_account,
-                    brand_role_code: employmentData.publishEmploymentInfo.brand_role_code,
-                    brand_guid: employmentData.publishEmploymentInfo.brand_guid,
-                    employee_user_account: guid,
-                    employer_time: employmentData.publishEmploymentInfo.create_time,
-                    audit_user_account: audit_user_account,
-                    deadline: deadline,
-                    status: "未审核"
-                  }, {
-                    transaction: t
-                  }),
-                  employment_detail.bulkCreate([{
-                    employment_guid: employmentData.guid,
-                    key: 'headImg',
-                    value: data['headImg']
-                  }, {
-                    employment_guid: employmentData.guid,
-                    key: 'name',
-                    value: data['name']
-                  }, {
-                    employment_guid: employmentData.guid,
-                    key: 'wechat',
-                    value: data['wechat']
-                  }, {
-                    employment_guid: employmentData.guid,
-                    key: 'cellphone',
-                    value: data['cellphone']
-                  }, {
-                    employment_guid: employmentData.guid,
-                    key: 'IDType',
-                    value: data['IDType']
-                  }, {
-                    employment_guid: employmentData.guid,
-                    key: 'IDNumber',
-                    value: data['IDNumber']
-                  }, {
-                    employment_guid: employmentData.guid,
-                    key: 'address',
-                    value: data['address']
-                  }, {
-                    employment_guid: employmentData.guid,
-                    key: 'addressDetail',
-                    value: data['addressDetail']
-                  }], {
-                    transaction: t
-                  })
-                ]).then(function() {
-                  t.commit()
-                  req.session.pwd = pwd
-                  return true
-                }).catch(function(err) {
-                  // err 是事务回调中使用promise链中的异常结果
-                  t.rollback()
-                  return Promise.reject("后台数据添加异常，申请失败")
-                })
-
-            })
+      //查找发起招募信息
+      publish_employment.findOne({
+        where: {
+          guid: publishEmploymentGuid
         }
       })
+    ]).then(function(result) {
+      var overtime = false
+
+      if (result[3] != null) {
+        var startDate = new Date(result[3].create_time)
+        var endDate = new Date(startDate.getTime() + 2 * 3600 * 1000)
+        if (endDate <= new Date()) {
+          overtime = true
+        }
+      }
+
+      if (result[0] != null) {
+        if (result[0].status == "未审核" || result[0].audit_result == "已通过") {
+          return Promise.reject("您已提交申请或已成为该品牌成员，申请失败")
+        }
+      } else if (result[1] != null) {
+        return Promise.reject("您已提交申请或已成为该品牌成员，申请失败")
+      } else if (result[2] == null) {
+        return Promise.reject("招募信息读取出错")
+      } else if (result[3] == null) {
+        return Promise.reject("招募信息读取出错")
+      } else if (result[3].status == false || overtime) {
+        return Promise.reject("招募已关闭，请关闭本页面")
+      }
+
+      var pwd = "";
+      for (var i = 0; i < 8; i++) {
+        pwd += Math.floor(Math.random() * 10);
+      }
+
+      var audit_user_account = result[2].user_account
+
+      return sequelize.transaction().then(function(t) {
+        return Promise.all([
+          user.create({
+            account: guid,
+            password: pwd
+          }, {
+            transaction: t
+          }),
+          user_role.create({
+            user_account: guid,
+            role_code: 'user'
+          }, {
+            transaction: t
+          }),
+          employment.create({
+            guid: employmentData.guid,
+            publish_employment_guid: employmentData.publishEmploymentInfo.guid,
+            employer_user_account: employmentData.publishEmploymentInfo.employer_user_account,
+            brand_role_code: employmentData.publishEmploymentInfo.brand_role_code,
+            brand_guid: employmentData.publishEmploymentInfo.brand_guid,
+            employee_user_account: guid,
+            employer_time: employmentData.publishEmploymentInfo.create_time,
+            audit_user_account: audit_user_account,
+            deadline: deadline,
+            status: "未审核"
+          }, {
+            transaction: t
+          }),
+          employment_detail.bulkCreate([{
+            employment_guid: employmentData.guid,
+            key: 'headImg',
+            value: data['headImg']
+          }, {
+            employment_guid: employmentData.guid,
+            key: 'name',
+            value: data['name']
+          }, {
+            employment_guid: employmentData.guid,
+            key: 'wechat',
+            value: data['wechat']
+          }, {
+            employment_guid: employmentData.guid,
+            key: 'cellphone',
+            value: data['cellphone']
+          }, {
+            employment_guid: employmentData.guid,
+            key: 'IDType',
+            value: data['IDType']
+          }, {
+            employment_guid: employmentData.guid,
+            key: 'IDNumber',
+            value: data['IDNumber']
+          }, {
+            employment_guid: employmentData.guid,
+            key: 'address',
+            value: data['address']
+          }, {
+            employment_guid: employmentData.guid,
+            key: 'addressDetail',
+            value: data['addressDetail']
+          }], {
+            transaction: t
+          })
+        ]).then(function() {
+          t.commit()
+          req.session.pwd = pwd
+          return true
+        }).catch(function(err) {
+          // err 是事务回调中使用promise链中的异常结果
+          t.rollback()
+          return Promise.reject("后台数据添加异常，申请失败")
+        })
+
+      })
+
+    })
   },
   getPwd(req, res, next) {
     return req.session.pwd
@@ -360,9 +361,9 @@ var exec = {
         return Promise.reject("招募信息读取出错")
       } else if (result[2].status == false || overtime) {
         return Promise.reject("招募已关闭，请关闭本页面")
-      } else {
-        return true
       }
+      return true
+
     })
   }
 }
