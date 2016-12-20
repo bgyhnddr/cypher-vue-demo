a841914f05d2e2044e26ad93517f5b81939f5f9d
bili.ou <bili.ou@cyphertech.cn>
2016-12-20 14:47:49 +0800

    test

--- src/components/PMP/EditProductLabel.vue
+++ src/components/PMP/EditProductLabel.vue
@@ -106,6 +106,7 @@ export default {
   },
   methods: {
     headerGoBack() {
+      //
       this.inputDate.chooseLabelItems = []
       this.inputDate.inputLabel = null
       this.showInputModel = true
