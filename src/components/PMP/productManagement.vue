553e5700da8e3898d686e3a4cd71427fa9dc8660
limt <921136839@qq.com>
2016-12-26 14:16:58 +0800

    修改我的货品顶栏返回按钮

--- src/components/PMP/productManagement.vue
+++ src/components/PMP/productManagement.vue
@@ -1,7 +1,10 @@
 <template>
 <div>
   <div>
-    <x-header :left-options="{showBack: true,backText:'返回'}" @click="onClickBack">我的货品</x-header>
+    <div class="vux-demo-header-box wapmain-header" slot="header">
+      <x-header :left-options="{showBack: false}">我的货品</x-header>
+      <div slot="left" class="onclick-back" @click="onClickBack">返回</div>
+    </div>
     <group>
       <!--货品管理-->
       <a class="weui_cell" v-link="{path: '/productManagement/productSetting'}">
