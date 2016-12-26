0b7ee669fdc3529b7bcd2aaa52b5dc7b7fad671c
limt <921136839@qq.com>
2016-12-26 14:16:58 +0800

    Signed-off-by: limt <921136839@qq.com>

---
+++ src/components/PMP/ProductRelate.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div>
+    <x-header :left-options="{showBack: true,backText:'返回'}" @click="onClickBack"></x-header>
+    <group>
+
+    </group>
+  </div>
+</div>
+</template>
+
+<script>
+import {
+  Group,
+  Alert,
+  XHeader
+} from 'vux'
+
+
+export default {
+  components: {
+    Group,
+    Alert,
+    XHeader
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    onClickBack() {
+      this.$route.router.go('/productManagement/linkProductToQRCode')
+    }
+  },
+  ready() {
+
+  }
+}
+</script>
