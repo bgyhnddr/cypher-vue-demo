90c176f1eb198d157e6a8f29abd2dc2178742f3f
bili.ou <bili.ou@cyphertech.cn>
2016-12-27 10:52:56 +0800

    add 货品关联页

--- src/components/PMP/LinkProductToQRCode.vue
+++ src/components/PMP/LinkProductToQRCode.vue
@@ -17,13 +17,13 @@
           <div>
             <label>{{$index + 1}} .</label>
           </div>
-          <img slot="icon" width="50" :src="getProductImgHref(productItem.pmp_variants[0].pmp_variant_images[0].attachment_id)" alt="产品图片" />
+          <img slot="icon" width="50" :src="getProductImgHref(productItem.pmp_variant.pmp_variant_images[0].attachment_id)" alt="产品图片" />
           <div>
             <label>{{productItem.pmp_variant.pmp_product.name}}</label>
             <label>{{productItem.pmp_variant.name}}</label>
             <label>{{productItem.name}}</label>
           </div>
-          <x-button @click="runWxScanQRCode">扫码</x-button>
+          <x-button @click="runWxScanQRCode(productItem)">扫码</x-button>
         </div>
         <div v-show="showModel.showPullUpSlot" slot="pullup" class="xs-plugin-pullup-container xs-plugin-pullup-up">
           <span v-show="pullUpScroller.pullupStatus === 'default'">{{pullUpScroller.pullupConfig.content}}</span>
@@ -36,7 +36,12 @@
     </div>
   </div>
   <div>
+    <confirm :show.sync="alert.showComfirm" title="" confirm-text="否" cancel-text="是" @on-cancel="showProductLinkedDetailPage">
+      <p style="text-align:center;">是否将箱 {{alert.comfirmMsg}}
+        <label>(规格)</label> 里？</p>
+    </confirm>
     <alert :show.sync="alert.showCatchError" button-text="确认" @on-hide="errorHandled">{{alert.catchErrorMsg}}</alert>
+    <alert :show.sync="alert.showErrorNoHandled" button-text="确认">{{alert.errorMsgNoHandled}}</alert>
   </div>
 </div>
 </template>
@@ -46,7 +51,8 @@ import {
   XHeader,
   XButton,
   Scroller,
-  Alert
+  Alert,
+  Confirm
 } from 'vux'
 import pmpProductAPI from '../../api/pmp_product'
 
@@ -55,7 +61,8 @@ export default {
     XHeader,
     XButton,
     Scroller,
-    Alert
+    Alert,
+    Confirm
   },
   data() {
     return {
@@ -70,7 +77,8 @@ export default {
           list: []
         },
         page: 0,
-        chooseTab: 1
+        chooseTab: 1,
+        chooseProduct: null
       },
       showModel: {
         showProductContainer: false,
@@ -88,7 +96,11 @@ export default {
       },
       alert: {
         showCatchError: false,
-        catchErrorMsg: null
+        catchErrorMsg: null,
+        showComfirm: false,
+        comfirmMsg: null,
+        showErrorNoHandled: false,
+        errorMsgNoHandled: null
       }
     }
   },
@@ -106,7 +118,7 @@ export default {
 
       pmpProductAPI.getSpecifications({
         page: this.productsData.page,
-        count:2,
+        count: 2,
         on_sell: this.productsData.chooseTab
       }).then(function(result) {
         if (result.list.length == 0) {
@@ -134,7 +146,7 @@ export default {
 
       pmpProductAPI.getSpecifications({
         page: this.productsData.page,
-        count:2,
+        count: 2,
         on_sell: this.productsData.chooseTab
       }).then(function(result) {
 
@@ -157,17 +169,36 @@ export default {
         that.alert.catchErrorMsg = "读取我的货品信息异常，请稍后再试"
       })
     },
-    goToEditProduct(productId) {
-      console.log("打开货品关联页")
+    runWxScanQRCode(productItem) {
+      console.log("扫码")
+
+      var boxCode = ""
+      this.linkToQRCode(boxCode, productItem)
+    },
+    linkToQRCode(boxCode, productItem) {
+      var checkFlag = this.checkBoxCode(boxCode)
+      if (checkFlag) {
+        this.alert.showComfirm = true
+        this.alert.comfirmMsg = boxCode + " 关联到" + productItem.pmp_variant.pmp_product.name +
+          " " + productItem.pmp_variant.name + " " + productItem.name
+        this.productsData.chooseProduct = productItem
+      } else {
+        alert.showErrorNoHandled = true
+        alert.errorMsgNoHandled = "扫码结果错误，请重新再扫一次"
+      }
+    },
+    checkBoxCode(boxCode) {
+      return true
     },
     openSearchPage() {
       this.$route.router.go("/productManagement/linkProductToQRCodeSearch")
     },
+    showProductLinkedDetailPage() {
+      console.log(JSON.stringify(this.productsData.chooseProduct))
+      // this.$route.router.go("/productManagement/linkProductToQRCodeSearch")
+    },
     getProductImgHref(fileId) {
       return '/service/public/upload/getAttachment?id=' + fileId
-    },
-    runWxScanQRCode(){
-      console.log("扫码")
     },
     errorHandled() {
       this.$route.router.go("/productManagement")
