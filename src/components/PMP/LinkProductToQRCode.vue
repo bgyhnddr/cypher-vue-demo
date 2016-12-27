72430f02a36eb13780d88cbee392edbbce0213ec
bili.ou <bili.ou@cyphertech.cn>
2016-12-27 11:37:23 +0800

    add 关联货品页，关联货品搜索页

--- src/components/PMP/LinkProductToQRCode.vue
+++ src/components/PMP/LinkProductToQRCode.vue
@@ -23,7 +23,7 @@
             <label>{{productItem.pmp_variant.name}}</label>
             <label>{{productItem.name}}</label>
           </div>
-          <x-button @click="runWxScanQRCode(productItem)">扫码</x-button>
+          <x-button @click="goToProductRelatePage(productItem.pmp_variant.id)">扫码</x-button>
         </div>
         <div v-show="showModel.showPullUpSlot" slot="pullup" class="xs-plugin-pullup-container xs-plugin-pullup-up">
           <span v-show="pullUpScroller.pullupStatus === 'default'">{{pullUpScroller.pullupConfig.content}}</span>
@@ -36,12 +36,7 @@
     </div>
   </div>
   <div>
-    <confirm :show.sync="alert.showComfirm" title="" confirm-text="否" cancel-text="是" @on-cancel="showProductLinkedDetailPage">
-      <p style="text-align:center;">是否将箱 {{alert.comfirmMsg}}
-        <label>(规格)</label> 里？</p>
-    </confirm>
     <alert :show.sync="alert.showCatchError" button-text="确认" @on-hide="errorHandled">{{alert.catchErrorMsg}}</alert>
-    <alert :show.sync="alert.showErrorNoHandled" button-text="确认">{{alert.errorMsgNoHandled}}</alert>
   </div>
   <div class="all-footer">© 2016 ShareWin.me 粤ICP备14056388号</div>
 </div>
@@ -52,8 +47,7 @@ import {
   XHeader,
   XButton,
   Scroller,
-  Alert,
-  Confirm
+  Alert
 } from 'vux'
 import pmpProductAPI from '../../api/pmp_product'
 
@@ -62,8 +56,7 @@ export default {
     XHeader,
     XButton,
     Scroller,
-    Alert,
-    Confirm
+    Alert
   },
   data() {
     return {
@@ -77,8 +70,7 @@ export default {
           end: null,
           list: []
         },
-        page: 0,
-        chooseProduct: null
+        page: 0
       },
       showModel: {
         showProductContainer: false,
@@ -96,11 +88,7 @@ export default {
       },
       alert: {
         showCatchError: false,
-        catchErrorMsg: null,
-        showComfirm: false,
-        comfirmMsg: null,
-        showErrorNoHandled: false,
-        errorMsgNoHandled: null
+        catchErrorMsg: null
       }
     }
   },
@@ -165,33 +153,11 @@ export default {
         that.alert.catchErrorMsg = "读取我的货品信息异常，请稍后再试"
       })
     },
-    runWxScanQRCode(productItem) {
-      console.log("扫码")
-
-      var boxCode = ""
-      this.linkToQRCode(boxCode, productItem)
-    },
-    linkToQRCode(boxCode, productItem) {
-      var checkFlag = this.checkBoxCode(boxCode)
-      if (checkFlag) {
-        this.alert.showComfirm = true
-        this.alert.comfirmMsg = boxCode + " 关联到" + productItem.pmp_variant.pmp_product.name +
-          " " + productItem.pmp_variant.name + " " + productItem.name
-        this.productsData.chooseProduct = productItem
-      } else {
-        alert.showErrorNoHandled = true
-        alert.errorMsgNoHandled = "扫码结果错误，请重新再扫一次"
-      }
-    },
-    checkBoxCode(boxCode) {
-      return true
+    goToProductRelatePage(productVariantId) {
+      this.$route.router.go("/productManagement/productRelate/" + productVariantId)
     },
     openSearchPage() {
       this.$route.router.go("/productManagement/linkProductToQRCodeSearch")
-    },
-    showProductLinkedDetailPage() {
-      console.log(JSON.stringify(this.productsData.chooseProduct))
-      // this.$route.router.go("/productManagement/linkProductToQRCodeSearch")
     },
     getProductImgHref(fileId) {
       return '/service/public/upload/getAttachment?id=' + fileId
