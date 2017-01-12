c84d6057eb8e65be03d030d3a1c7ff009d9e8a29
limt <921136839@qq.com>
2017-01-12 16:11:35 +0800

    创建我的团队主页

---
+++ src/components/TEAM/TeamManagement.vue
@@ -0,0 +1,84 @@
+<template>
+<div id="teamManagement">
+  <div>
+    <div class="vux-demo-header-box wapmain-header" slot="header">
+      <x-header :left-options="{showBack: false}">我的团队</x-header>
+      <div slot="left" class="onclick-back" @click="onClickBack">返回</div>
+    </div>
+    <group>
+      <!--冻结团队成员-->
+      <a class="weui_cell a-li a-li-first" v-link="{path: ''}">
+        <div class="weui_cell_hd">
+          <img src="">
+        </div>
+        <div class="weui_cell_bd weui_cell_primary">
+          <p>冻结团队成员</p>
+        </div>
+        <div class="weui_cell_ft" :class="{'with_arrow': true}">
+          <slot name="value"></slot>
+          <slot></slot>
+        </div>
+      </a>
+      <!--提拔团队成员-->
+      <a class="weui_cell a-li-last"  v-link="{path: ''}">
+        <div class="weui_cell_hd">
+            <img src="">
+        </div>
+        <div class="weui_cell_bd weui_cell_primary">
+          <p>提拔团队成员</p>
+        </div>
+        <div class="weui_cell_ft" :class="{'with_arrow': true}">
+          <slot name="value"></slot>
+          <slot></slot>
+        </div>
+      </a>
+    </group>
+  </div>
+</div>
+<div class="all-footer">© 2016 ShareWin.me 粤ICP备14056388号</div>
+</template>
+
+<script>
+import {
+  Group,
+  Alert,
+  XHeader
+} from 'vux'
+import authAPI from '../../api/auth'
+
+export default {
+  components: {
+    Group,
+    Alert,
+    XHeader
+  },
+  data() {
+    return {
+      user: {
+        user_info: {}
+      }
+    }
+  },
+  methods: {
+    onClickBack() {
+      this.$route.router.go('/homePage')
+    },
+    getPersonalInfo() {
+      var that = this
+      authAPI.getUser().then(function(result) {
+        that.user.user_info = result
+      })
+    }
+  },
+  ready() {
+    this.getPersonalInfo()
+  }
+}
+</script>
+<style>
+body{
+  background-color:#f2f2f2!important
+}
+
+</style>
+</style>
