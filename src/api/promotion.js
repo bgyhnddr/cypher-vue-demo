6668d95702882d223fc4bca7ab44acac1ae060dd
bili.ou <bili.ou@cyphertech.cn>
2017-01-18 17:51:00 +0800

    添加promotion api

--- src/api/promotion.js
+++ src/api/promotion.js
@@ -5,7 +5,16 @@ export default {
   getPromotionOperableLevels() {
       return httpRequest.get(path + 'getPromotionOperableLevels')
   },
-  getOperableStaffs(params) {
-      return httpRequest.get(path + 'getOperableStaffs',params)
+  getPromotionOperableStaffs(params) {
+      return httpRequest.get(path + 'getPromotionOperableStaffs',params)
   },
+  getLevels(params) {
+      return httpRequest.get(path + 'getLevels',params)
+  },
+  createPromotion(params) {
+      return httpRequest.post(path + 'createPromotion',params)
+  },
+  getPromotion(params) {
+      return httpRequest.get(path + 'getPromotion',params)
+  }
 }
