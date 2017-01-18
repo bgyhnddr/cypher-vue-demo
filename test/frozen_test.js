784895a056eebc804f0c72ed1590f0039ad210fd
limt <921136839@qq.com>
2017-01-18 11:35:12 +0800

    增加测试功能

--- test/frozen_test.js
+++ test/frozen_test.js
@@ -1,4 +1,6 @@
 var should = require('should')
+var role_permission = require('../db/models/role_permission')
+var permission = require('../db/models/permission')
 var employment_term = require('../db/models/employment_term')
 var team = require('../db/models/team')
 var team_agent = require('../db/models/team_agent')
@@ -11,11 +13,25 @@ describe('frozen-test', () => {
   before(function() {
     this.timeout(10000)
       // runs before all tests in this block
-    return Promise.all([
-      frozen_agent,
-    ].map((o) => o.sync({
-      force: true
-    }))).then((result) => {
+    return role_permission.findOne({
+      where: {
+        role_code: "user",
+        permission_code: "frozen"
+      }
+    }).then((result) => {
+      if (result == null) {
+        return Promise.all([
+          role_permission.create({
+            role_code: "user",
+            permission_code: "frozen"
+          }),
+          permission.create({
+            code: "frozen",
+            name: "frozen"
+          })
+        ])
+      }
+    }).then((result) => {
       return Promise.all([
         frozen_agent.create({
           agent_guid: "321"
@@ -41,7 +57,7 @@ describe('frozen-test', () => {
       return require('../server/private/frozen')({
         session: {
           userInfo: {
-            name: "tester"
+            name: "admin"
           }
         },
         params: {
@@ -54,10 +70,19 @@ describe('frozen-test', () => {
       console.log(e)
     }
   }
+  describe('getFrozenLevels', () => {
+    it('get all forzen levels', () => {
+      return testfunction("getFrozenLevels").then((result) => {
+        result.length.should.equal(4)
+        result[0].brand_role_name.should.equal("总代理")
+        result[1].number.should.be.above(0)
+      })
+    })
+  })
   describe('FrozenAgent', () => {
     it('frozen agent', () => {
       return testfunction("FrozenAgent", {
-        agent:"123"
+        agent: "123"
       }).then((result) => {
         return result.should.equal('OK')
       })
@@ -66,17 +91,18 @@ describe('frozen-test', () => {
   describe('ThawAgent', () => {
     it('thaw agent frozen', () => {
       return testfunction("ThawAgent", {
-        agent:"321"
+        agent: "321"
       }).then((result) => {
         return result.should.equal('OK')
       })
     })
     it('thaw agent not_frozen', () => {
       return testfunction("ThawAgent", {
-        agent:"321"
+        agent: "321"
       }).then((result) => {
         return result.should.equal('代理未被冻结')
       })
     })
   })
+
 })
