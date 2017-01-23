139d32ae4c9a52a0ae42c8b8cf324d43d34e5ada
hao.wang <hao.wang@cyphertech.cn>
2017-01-23 21:07:58 +0800

    从pmp_goods表获取商品数据

--- server/public/sale.js
+++ server/public/sale.js
@@ -1,10 +1,50 @@
-var pmp_server = require('../pmp_server')
+//var pmp_server = require('../pmp_server')
+var pmp_good = require('../../db/models/pmp_goods')
+var pmp_specification = require('../../db/models/pmp_specification')
+var pmp_variant = require('../../db/models/pmp_variant')
+var pmp_product = require('../../db/models/pmp_product')
+var pmp_product_price = require('../../db/models/pmp_product_price')
+
 var exec = {
   showPack(req, res, next) {
     var packcode = req.body.packcode
 
-    return pmp_server.show_pack(packcode)
+    pmp_good.belongsTo(pmp_specification)
+    pmp_specification.belongsTo(pmp_variant)
+    pmp_variant.belongsTo(pmp_product)
+    pmp_product.hasMany(pmp_product_price)
+
+    return pmp_good.findOne({
+      where: {
+        goods_code: packcode
+      },
+      include: {
+        model: pmp_specification,
+        include: {
+          model: pmp_variant,
+          include: {
+            model: pmp_product,
+            include: {
+              model: pmp_product_price,
+              where: { brand_role_code: '4' }
+            }
+          }
+        }
+      }
+    }).then((result) => {
+      return {
+        full_code: result.goods_code,
+        product_name: result.pmp_specification.pmp_variant.pmp_product.name,
+        retail_price: '￥' + result.pmp_specification.pmp_variant.pmp_product.pmp_product_prices[0].price.toFixed(2),
+        variant_name: result.pmp_specification.pmp_variant.name,
+        specification: result.pmp_specification.name,
+        sold_time: result.sold_time,
+        sold_by: result.seller_user_account,
+        sold: result.status === 'SOL'
+      }
+    })
   },
+
   packSoldBy(req, res, next) {
     var packcode = req.body.packcode
 
@@ -17,16 +57,29 @@ var exec = {
       where: {
         user_account: user_account
       }
-    }).then(function(result) {
+    }).then(function (result) {
       if (result == null) {
         return Promise.reject("获取您的个人资料读取出错")
       } else {
         var who = result.user_account
 
-        return pmp_server.pack_sold_by(packcode,who)
+        return pmp_good.findOne({
+          where: {
+            goods_code: packcode
+          }
+        }).then((good) => {
+          if (good) {
+            return good.update({
+              seller_user_account: user_account,
+              sold_time: new Date(),
+              status: 'SOL'
+            })
+          }
+        })
       }
     })
   },
+
   getAgentInfo(req, res, next) {
     var user_account = req.body.user_account
 
@@ -42,7 +95,7 @@ var exec = {
       include: [{
         model: agent_detail
       }]
-    }).then(function(result) {
+    }).then(function (result) {
       if (result == null) {
         return Promise.reject("销售员资料读取出错")
       } else {
@@ -56,11 +109,11 @@ var exec = {
 
 module.exports = (req, res, next) => {
   var action = req.params.action
-  Promise.resolve(action).then(function(result) {
+  Promise.resolve(action).then(function (result) {
     return exec[result](req, res, next)
-  }).then(function(result) {
+  }).then(function (result) {
     res.send(result)
-  }).catch(function(error) {
+  }).catch(function (error) {
     res.status(500).send(error.toString())
   })
 }
