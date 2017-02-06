0ea73f943775d5fa7feabb3960f1b38dc5cdd24c
guanyi.bei <guanyi.bei@cyphertech.cn>
2017-02-06 16:59:44 +0800

    temp commit
    Signed-off-by: guanyi.bei <guanyi.bei@cyphertech.cn>

---
+++ src/components/Cart.vue
@@ -0,0 +1,83 @@
+<template>
+<div>
+  <view-box v-ref:view-box>
+    <div slot="header">
+      <x-header @on-click-back="onClickBack" :left-options="leftOptions">采购清单</x-header>
+    </div>
+    <group>
+      <cell v-for="item in myCartList" :title="item.pmp_specification_name">
+        <div slot="child">
+          <span @click="toggleItem($index,item)">
+            <span v-if="item.checked">check</span>span</span>
+        </div>
+        <img slot="icon" width="50" src="" alt="产品图片" />
+      </cell>
+    </group>
+  </view-box>
+</div>
+</template>
+
+<script>
+import {
+  XHeader,
+  ViewBox,
+  Cell,
+  Group,
+  XInput
+} from 'vux'
+export default {
+  props: {
+    cartList: Array,
+    backPath: {
+      type: String,
+      default: "/Login"
+    }
+  },
+  components: {
+    XHeader,
+    ViewBox,
+    Cell,
+    Group,
+    XInput
+  },
+  data() {
+    return {
+      myCartList: [],
+      leftOptions: {
+        backText: "返回",
+        preventGoBack: true,
+        showBack: true
+      }
+    }
+  },
+  watch: {
+    cartList(val) {
+      if (val) {
+        this.myCartList = val.map((o) => {
+          return {
+            "pmp_specification_id": o.pmp_specification_id, //类型ID
+            "pmp_specification_name": o.pmp_specification_name, //类型名
+            "pmp_variant_name": o.pmp_variant_name, //规格名
+            "pmp_product": o.pmp_product, //产品名
+            "count": o.count, //数量
+            "price": o.price, //单价
+            "checked": false
+          }
+        })
+      }
+    }
+  },
+  methods: {
+    onClickBack() {
+      this.$router.go(this.backPath)
+    },
+    toggleItem(index, item) {
+      this.myCartList.$set(index, item)
+      this.$emit('on-change', this.myCartList)
+    }
+  },
+  ready() {
+    this.myCartList = this.cartList
+  }
+}
+</script>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,45 +1,32 @@
 <template>
-<button @click="scan">
-  扫一扫</button>
-<button @click="choose">上传</button>
-<img :src="imgId" alt="" />
-<div v-el:qr></div>
+<cart :cart-list="cartList" @on-change="change">
+</cart>
 </template>
 <script>
-var qrcanvas = require('qrcanvas')
-var request = require('../extend/http-request')
+import Cart from "./Cart"
 export default {
   data() {
     return {
-      test: false,
-      imgId: "",
-      str: "来吧中文"
+      cartList: [{
+        "pmp_specification_id": "", //类型ID
+        "pmp_specification_name": "类型名", //类型名
+        "pmp_variant_name": "", //规格名
+        "pmp_product": "", //产品名
+        "count": "", //数量
+        "price": "" //单价
+      }]
     }
   },
   methods: {
-    choose() {
-      var that = this
-      wx.chooseImage({
-        count: 1, // 默认9
-        sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
-        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-        success: function(res) {
-          that.imgId = res.localIds[0]; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-          wx.uploadImage({
-            localId: that.imgId, // 需要上传的图片的本地ID，由chooseImage接口获得
-            isShowProgressTips: 1, // 默认为1，显示进度提示
-            success: function(res) {
-              var serverId = res.serverId; // 返回图片的服务器端ID
-            }
-          })
-        }
-      })
+    change(list) {
+      this.cartList = list
     }
   },
+  components: {
+    Cart
+  },
   ready() {
-    this.$els.qr.appendChild(qrcanvas({
-      data: 'http://localhost:8080/#!/employManagement/brandAuthorization/2c2cd470-b21d-11e6-920e-6b9af5cfe498/%E8%B4%9D%E5%88%A9%E9%BE%99'
-    }))
+
   }
 }
 </script>
